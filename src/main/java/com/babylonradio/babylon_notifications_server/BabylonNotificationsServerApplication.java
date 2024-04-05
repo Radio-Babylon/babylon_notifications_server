95df93629bb4f894494a6d628ddfdd7f3bb749f4
EnzoLouni <enzol.babylon@gmail.com>
2024-04-05 17:57:02 +0200

    Init commit

---
+++ src/main/java/com/babylonradio/babylon_notifications_server/BabylonNotificationsServerApplication.java
@@ -0,0 +1,13 @@
+package com.babylonradio.babylon_notifications_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabylonNotificationsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabylonNotificationsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babylonradio/babylon_notifications_server/BabylonNotificationsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babylonradio.babylon_notifications_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabylonNotificationsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
